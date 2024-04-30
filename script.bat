c6906a256f542330e3c07d5b55129148cbc56cdb
Ricardo Oliveira <ricardomarino@live.com>
2024-04-30 16:30:32 -0300

    Entregando desafio

---
+++ script.bat
@@ -0,0 +1,15 @@
+echo "criando as imagens"
+
+docker build -t ricardomarino/backend:1.0 backend/.
+docker build -t ricardomarino/database:1.0 database/.
+
+echo "realizando push"
+docker push ricardomarino/backend:1.0
+docker push ricardomarino/database:1.0
+
+echo "services"
+kubectl apply -f ./service.yml
+
+echo "deployments"
+kubectl apply -f ./deployment.yml
+
